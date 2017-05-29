d52d784dc984c2aea9af614e0854eb50a29a3e86
andy stewart <andrew.stewart1210@gmail.com>
2017-05-29 21:06:32 +1200

    added power ups and scared mode to pacman

--- app/models/pacman.js
+++ app/models/pacman.js
@@ -3,7 +3,7 @@ var maze = [
     [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
     [0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0],
     [0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 0],
     [0, 2, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 2, 0],
     [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 0, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 0],
     [0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0],
@@ -15,7 +15,7 @@ var maze = [
     [0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0],
     [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
     [0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2, 0],
-    [0, 2, 2, 2, 2, 0, 2, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 2, 0, 2, 2, 2, 2, 0],
+    [0, 3, 2, 2, 2, 0, 2, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 2, 0, 2, 2, 2, 3, 0],
     [0, 0, 0, 0, 2, 0, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 0, 2, 0, 0, 0, 0],
     [0, 0, 0, 0, 2, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 0, 0, 0, 0],
     [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 0, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 0],
@@ -28,27 +28,30 @@ var dotsnumber = 0;
 
 var pac = { position: { row: 17*20, col: 12*20 }, direction: { act: null, req: null}, speed: 2.5};
 
-var ghost0 = { position: { row: 10*20, col: 11*20}, direction: { act: 'up', pref: [null, null, null, null], favour: [0,1,2,3]}, speed: 2};
-var ghost1 = { position: { row: 10*20, col: 13*20}, direction: { act: 'up', pref: [null, null, null, null], favour: [0,2,3,1]}, speed: 2};
-var ghost2 = { position: { row: 10*20, col: 10*20}, direction: { act: 'up', pref: [null, null, null, null], favour: [0,2,1,3]}, speed: 2};
-var ghost3 = { position: { row: 10*20, col: 14*20}, direction: { act: 'up', pref: [null, null, null, null], favour: [0,1,3,2]}, speed: 2};
+var ghost0 = { position: { row: 10*20, col: 11*20}, direction: { act: 'up', pref: [null, null, null, null], favour: [0,1,2,3]}, color: 'firebrick', dead: false, restart: null};
+var ghost1 = { position: { row: 10*20, col: 13*20}, direction: { act: 'up', pref: [null, null, null, null], favour: [0,2,3,1]}, color: 'aqua', dead: false, restart: null};
+var ghost2 = { position: { row: 10*20, col: 10*20}, direction: { act: 'up', pref: [null, null, null, null], favour: [0,2,1,3]}, color: 'orange', dead: false, restart: null};
+var ghost3 = { position: { row: 10*20, col: 14*20}, direction: { act: 'up', pref: [null, null, null, null], favour: [0,1,3,2]}, color: 'pink', dead: false, restart: null};
 
-var ghosts = [ ghost0, ghost1, ghost2, ghost3 ];
+var ghosts = { all: [ ghost0, ghost1, ghost2, ghost3 ], speed: 2, scared: false};
 var ghoststarts = [ {row: 10*20, col: 11*20}, {row:10*20, col:13*20}, {row: 10*20, col: 10*20}, {row: 10*20, col: 14*20}];
 
 var startpacposition = { row: 17*20, col: 12*20 };
 
+var powerup = null;
 interval = null;
 
 function reset() {
     dotsnumber = 0;
     pac.position.row = startpacposition.row;
     pac.position.col = startpacposition.col;
-    ghosts.forEach(function(ghost, index) {
+    ghosts.all.forEach(function(ghost, index) {
         ghost.position.row = ghoststarts[index].row;
         ghost.position.col = ghoststarts[index].col;
+        ghost.dead = false;
     });
     pac.direction = { act: null, req: null };
+    ghosts.scared = false;
     clearInterval(interval);
     $('#frame').html(render());
 }
@@ -99,15 +102,28 @@ function gameloop() {
 
     var collision = false;
 
-    ghosts.forEach(function(ghost) {
-        setghostdirection(ghost);
-        moveghost(ghost);
-        var rowcol = Math.abs(pac.position.row - ghost.position.row);
-        var colcol = Math.abs(pac.position.col - ghost.position.col);
+    ghosts.all.forEach(function(ghost, index) {
+        if (ghost.restart == null) {
+            setghostdirection(ghost);
+            moveghost(ghost);
+            if (!ghost.dead) {
+                var rowcol = Math.abs(pac.position.row - ghost.position.row);
+                var colcol = Math.abs(pac.position.col - ghost.position.col);
+                if (rowcol < 20 && colcol < 20 && (rowcol < 16 && colcol < 16)) {
+                    if (ghosts.scared) {
+                        $('#ghost' + index).css({'opacity':'0.4'});
+                        ghost.dead = true;
+                    } else {
+                        collision = true;
+                    }
+                }
+            } else {
+                if (ghost.position.row == 10*20 && ghost.position.col == 12*20) {
+                    setghostrestart(ghost, index);
+                }
+            }
+        }     
         
-        if (rowcol < 20 && colcol < 20 && (rowcol < 16 && colcol < 16)) {
-            collision = true;
-        }
     });
 
     printCharacters();
@@ -139,7 +155,7 @@ function setpacdirection(event) {
 
 function removeCharacters() {
     $('#r' + pac.position.row + 'c' + pac.position.col).removeClass().addClass('pc_node');
-    ghosts.forEach(function(ghost) {
+    ghosts.all.forEach(function(ghost) {
         $('#r' + ghost.position.row + 'c' + ghost.position.col).removeClass().addClass('pc_node');
         $('#r' + ghost.position.row + 'c' + ghost.position.col).children().show();
     });
@@ -152,11 +168,14 @@ function printCharacters() {
     if ($('#r' + Math.floor(pac.position.row/20) + 'c' + Math.floor(pac.position.col/20)).find('.pc_dot').length!=0) {
         if (pac.position.row%20==0 && pac.position.col%20==0) {
             dotsnumber --;
+            if ($('#r' + Math.floor(pac.position.row/20) + 'c' + Math.floor(pac.position.col/20)).find('.pc_powerup').length!=0) {
+                setpowerup();
+            }
             $('#r' + Math.floor(pac.position.row/20) + 'c' + Math.floor(pac.position.col/20)).empty();
         }
     }
-    ghosts.forEach(function(ghost, index) {
-        $('#ghost' + index).css({ 'top': ghost.position.row + 'px', 'left': ghost.position.col + 'px'});
+    ghosts.all.forEach(function(ghost, index) {
+        $('#ghost' + index).css({'top': ghost.position.row + 'px', 'left': ghost.position.col + 'px'});
     });
     
 }
@@ -261,53 +280,63 @@ function trydown(pos) {
 }
 
 function moveghost(ghost) {
+    var speed = ghosts.speed;
+    if (ghost.dead) {
+        speed = 1;
+    }
     switch (ghost.direction.act) {
-            case 'left':
-                left(ghost.position, ghost.speed);
-                break;
-            case 'right':
-                right(ghost.position, ghost.speed);
-                break;
-            case 'up':
-                up(ghost.position, ghost.speed);
-                break;
-            case 'down':
-                down(ghost.position, ghost.speed);
-                break;
-        }
+        case 'left':
+            left(ghost.position, speed);
+            break;
+        case 'right':
+            right(ghost.position, speed);
+            break;
+        case 'up':
+            up(ghost.position, speed);
+            break;
+        case 'down':
+            down(ghost.position, speed);
+            break;
+    }
 }
 
 function setghostdirection(ghost) {
-    ghost.direction.pref = findghostpreferreddirection(ghost.position);
+    ghost.direction.pref = findghostpreferreddirection(ghost.position, ghost.dead);
 
     var l = tryleft(ghost.position);
     var r = tryright(ghost.position);
     var u = tryup(ghost.position);
     var d = trydown(ghost.position);
 
-    var dirset = false;
-    var p = 0;
     switch (ghost.direction.act) {
         case 'left':
-            ghost.direction.act = getghostdirection(ghost.direction, l, !l&&!u&&!d && r, u, d);
+            ghost.direction.act = getghostdirection(ghost, l, !l&&!u&&!d && r, u, d);
             break;
         case 'right':
-            ghost.direction.act = getghostdirection(ghost.direction, !r&&!u&&!d && l, r, u, d);
+            ghost.direction.act = getghostdirection(ghost, !r&&!u&&!d && l, r, u, d);
             break;
         case 'up':
-            ghost.direction.act = getghostdirection(ghost.direction, l, r, u, !l&&!r&&!u && d);
+            ghost.direction.act = getghostdirection(ghost, l, r, u, !l&&!r&&!u && d);
             break;
         case 'down':
-            ghost.direction.act = getghostdirection(ghost.direction, l, r, !l&&!r&&!d && u, d);
+            ghost.direction.act = getghostdirection(ghost, l, r, !l&&!r&&!d && u, d);
             break;
     }
 }
 
-function getghostdirection(dir, l, r, u, d) {
+function getghostdirection(ghost, l, r, u, d) {
+    var dir = ghost.direction;
     var act = null;
     var p = 0;
+    var array = ghost.direction.favour.slice(0);
+    if (ghost.dead) {
+        array = [0,1,2,3];
+    } else if (ghosts.scared) {
+        array = array.reverse();
+    } 
+
     while (act==null && p < 4) {
-        switch (dir.pref[dir.favour[p]]) {
+        switch (dir.pref[array[p]]) {
             case 'left':
                 if (l) {
                     act = 'left';
@@ -334,8 +363,12 @@ function getghostdirection(dir, l, r, u, d) {
     return act;
 }
 
-function findghostpreferreddirection(pos) {
+function findghostpreferreddirection(pos, dead) {
     delta = { row: pos.row - pac.position.row, col: pos.col - pac.position.col};
+    if (dead) {
+        delta.row = pos.row - (10*20);
+        delta.col = pos.col - (12*20);
+    }
     var pref = [null, null, null, null];
 
     // top left
@@ -399,6 +432,50 @@ function findghostpreferreddirection(pos) {
         return pref;
     }
     return pref;
+}
+
+function setpowerup() {
+    ghosts.scared = true;
+    ghosts.speed = 1;
+    ghosts.all.forEach(function(ghost, index) {
+        $('#ghost' + index).css({'background-color': 'var(--starrynight)'});
+    });
+    if (powerup != null) {
+        clearTimeout(powerup);
+    }
+    powerup = setTimeout(function() {
+        ghosts.scared = false;
+        ghosts.all.forEach(function(ghost, index) {
+            if (ghost.position.row % 2 == 1) {
+                moveghost(ghost);
+            }
+            if (ghost.position.col % 2 == 1) {
+                moveghost(ghost);
+            }
+        });
+        ghosts.speed = 2;
+        ghosts.all.forEach(function(ghost, index) {
+            if (!ghost.dead) {
+                $('#ghost' + index).css({'background-color': ghost.color});
+            }
+        });
+        powerup = null;
+    }, 4000);
+}
+
+function setghostrestart(ghost, index) {
+    ghost.dead = false;
+    var color = ghost.color;
+    if (ghosts.scared) {
+        color = 'var(--starrynight)';
+    }
+    $('#ghost' + index).css({'opacity': '1', 'background-color': color});
+    if (ghost.restart != null) {
+        clearTimeout(ghost.restart);
+    }
+    ghost.restart = setTimeout(function() {
+        ghost.restart = null;
+    }, 1000);
 }
 
 
@@ -476,13 +553,16 @@ function render() {
             } else if (maze[i][j] == 2) {
                 content += '<div class="pc_node" id="r' + i + 'c' + j + '"><div class="pc_dot"></div></div>'
                 dotsnumber++;
+            } else if (maze[i][j] == 3) {
+                content += '<div class="pc_node" id="r' + i + 'c' + j + '"><div class="pc_dot pc_powerup"></div></div>'
+                dotsnumber++;
             }
         }
         content += '</div>'
     }
     content += '<div id="pac" class="pc_pac" style="top:' + pac.position.row + 'px;left:' + pac.position.col + 'px;"/>';
-    ghosts.forEach(function(ghost, index) {
-        content += '<div id="ghost'+index+'" class="pc_ghost pc_g' + index + '" style="top:' + ghost.position.row + 'px;left:' + ghost.position.col + 'px;"/>';
+    ghosts.all.forEach(function(ghost, index) {
+        content += '<div id="ghost'+index+'" class="pc_ghost" style="background-color:'+ghost.color+';top:' + ghost.position.row + 'px;left:' + ghost.position.col + 'px;"/>';
     });
     return content;
 }
